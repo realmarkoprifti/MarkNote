38f78cce6d747165d94bbfb977cf93d1d8581a3a
therealmarkoprifti <marko.prifti4@gmail.com>
2023-12-16 21:06:47 +0100

    Add EmptyNotes

---
+++ src/components/EmptyNotes.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@nextui-org/react";
+import React from "react";
+import { FaNotesMedical } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa";
+
+const EmptyNotes = () => {
+  return (
+    <div className="flex justify-center items-center flex-col gap-3">
+      <span className="text-9xl">
+        <FaNotesMedical />
+      </span>
+      <br />
+      <span className="text-lg">Cmon! Go and create a new note!</span>
+      <Button startContent={<FaPlus />} color="primary">
+        Create Note
+      </Button>
+    </div>
+  );
+};
+
+export default EmptyNotes;
